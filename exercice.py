ea70cd8e58fa843e590fba1e37c9c22a79d9b07f
Alex HC <hcalex1.accounts@hotmail.com>
2021-06-12 10:55:50 -0400

    idk lol

--- exercice.py
+++ exercice.py
@@ -8,11 +8,15 @@ import numpy as np
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+    return np.linspace(-1.3, 2.5, num=64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    angles = np.sum(np.arctan(cartesian_coordinates), axis=1)
+    radius = np.hypot(cartesian_coordinates[:, 0], cartesian_coordinates[:, 1])
+    print(angles)
+    print(radius)
+    return np.concatenate([angles, radius], axis=1)
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
@@ -21,5 +25,7 @@ def find_closest_index(values: np.ndarray, number: float) -> int:
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
-    pass
+    print(linear_values())
+    print(coordinate_conversion(np.array([[1, 0],
+                                          [0, 1],
+                                          [1, 1]])))
